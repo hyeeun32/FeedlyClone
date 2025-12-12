fc0826c1ccf42d54f0eaab5bc10bbb37e6a139e9
김태원 <kronak2355@gmail.com>
2025-12-12 17:19:15 +0900

    first commit

---
+++ src/main/kotlin/com/feedly/feedlyclonebackend/FeedlyCloneBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.feedly.feedlyclonebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeedlyCloneBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<FeedlyCloneBackendApplication>(*args)
+}
